ecd46e329171298c13ce40695c100fd976fe96ff
alxeli <alexelicarlson@gmail.com>
2015-12-18 15:30:07 -0800

    Added database create script and stored procedure script.  added 2 stored procedures.

---
+++ Documentation/Database/dbCreate.sql
@@ -0,0 +1,125 @@
+--create 
+
+USE master;
+GO
+
+IF  DB_ID('D3BuildMark') IS NOT NULL
+DROP DATABASE D3BuildMark;
+GO
+
+CREATE DATABASE D3BuildMark;
+GO
+
+USE D3BuildMark;
+
+CREATE TABLE Skill
+(
+	SkillID			int				CONSTRAINT	Skill_PK			PRIMARY KEY IDENTITY,
+	Name			varchar(60)		NOT NULL,
+	Description		varchar(200)	NOT NULL
+);
+CREATE TABLE SkillList
+(
+	SkillListID		int				CONSTRAINT	SkillList_PK		PRIMARY KEY IDENTITY,
+	Active0ID		int				CONSTRAINT	A0_FK_SL			REFERENCES Skill(SkillID),
+	Active1ID		int				CONSTRAINT	A1_FK_SL			REFERENCES Skill(SkillID),
+	Active2ID		int				CONSTRAINT	A2_FK_SL			REFERENCES Skill(SkillID),
+	Active3ID		int				CONSTRAINT	A3_FK_SL			REFERENCES Skill(SkillID),
+	Active4ID		int				CONSTRAINT	A4_FK_SL			REFERENCES Skill(SkillID),
+	Active5ID		int				CONSTRAINT	A5_FK_SL			REFERENCES Skill(SkillID),
+	Passive0ID		int				CONSTRAINT	P0_FK_SL			REFERENCES Skill(SkillID),
+	Passive1ID		int				CONSTRAINT	P1_FK_SL			REFERENCES Skill(SkillID),
+	Passive2ID		int				CONSTRAINT	P2_FK_SL			REFERENCES Skill(SkillID),
+	Passive3ID		int				CONSTRAINT	P3_FK_SL			REFERENCES Skill(SkillID)
+);
+
+CREATE TABLE Item
+(
+	ItemID			int				CONSTRAINT	Item_PK				PRIMARY KEY IDENTITY,
+	Name			varchar(60)		NOT NULL,
+	Attributes		varchar(500)	NOT NULL
+);
+CREATE TABLE ItemList
+(
+	ItemListID		int				CONSTRAINT	ItemList_PK		PRIMARY KEY IDENTITY,
+	HeadID			int				CONSTRAINT	Head_FK_IL		REFERENCES Item(ItemID),
+	NeckID			int				CONSTRAINT	Neck_FK_IL		REFERENCES Item(ItemID),
+	ShouldersID		int				CONSTRAINT	Shoulders_FK_IL	REFERENCES Item(ItemID),
+	GlovesID		int				CONSTRAINT	Gloves_FK_IL	REFERENCES Item(ItemID),
+	ChestID			int				CONSTRAINT	Chest_FK_IL		REFERENCES Item(ItemID),
+	BracersID		int				CONSTRAINT	Bracers_FK_IL	REFERENCES Item(ItemID),
+	BeltID			int				CONSTRAINT	Belt_FK_IL		REFERENCES Item(ItemID),
+	LeftRingID		int				CONSTRAINT	LeftRing_FK_IL	REFERENCES Item(ItemID),
+	RightRingID		int				CONSTRAINT	RightRing_FK_IL	REFERENCES Item(ItemID),
+	PantsID			int				CONSTRAINT	Pants_FK_IL		REFERENCES Item(ItemID),
+	BootsID			int				CONSTRAINT	Boots_FK_IL		REFERENCES Item(ItemID),
+	LeftHandID		int				CONSTRAINT	LeftHand_FK_IL	REFERENCES Item(ItemID),
+	RightHandID		int				CONSTRAINT	RightHand_FK_IL	REFERENCES Item(ItemID)
+);
+
+CREATE TABLE BuildMark
+(
+	BuildMarkID		int				CONSTRAINT	BuildMark_PK		PRIMARY KEY IDENTITY,
+  --BuildSnapshotID	int				CONSTRAINT	BuildSnapshot_FK 	REFERENCES BuildSnapshot(BuildSnapshotID), (added later)
+	Score			varchar(20)		NOT NULL,
+	Damage			varchar(20)		NOT NULL,
+	Toughness		varchar(20)		NOT NULL,
+	Date			DateTime		NOT NULL
+);
+
+CREATE TABLE BuildSnapshot
+(
+	BuildSnapshotID	int				CONSTRAINT	BuildSnapshot_PK	PRIMARY KEY IDENTITY,
+	BuildMarkID		int				CONSTRAINT	BuildMark_FK_BS		REFERENCES BuildMark(BuildMarkID),
+	SkillListID		int				CONSTRAINT	SkillList_FK_BS		REFERENCES SkillList(SkillListID),
+	ItemListID		int				CONSTRAINT	ItemList_FK_BS		REFERENCES ItemList(ItemListID),
+);
+
+--now add that BuildMark foreign key...
+ALTER TABLE BuildMark
+ADD BuildSnapshotID int
+GO
+
+ALTER TABLE BuildMark
+ADD CONSTRAINT BuildSnapshot_FK
+FOREIGN KEY (BuildSnapshotID)
+REFERENCES BuildSnapshot(BuildSnapshotID)
+GO
+
+CREATE TABLE Hero
+(
+	HeroID			int				CONSTRAINT	Hero_PK				PRIMARY KEY IDENTITY,
+	BuildSnapshotID	int				CONSTRAINT	BuildSnapshot_FK_H 	REFERENCES BuildSnapshot(BuildSnapshotID),
+	HeroName		varchar(12)		NOT NULL,
+	HeroClass		varchar(12)		NOT NULL
+);
+
+CREATE TABLE UserProfile
+(
+	UserID			int				CONSTRAINT	User_PK				PRIMARY KEY IDENTITY,
+	HeroID			int				CONSTRAINT	Hero_FK_UP			REFERENCES Hero(HeroID),
+	UserName		varchar(40)		NOT NULL,
+	Password		varchar(40)		NOT NULL,
+	Email			varchar(40)		NOT NULL,
+	Battletag		varchar(40)		NOT NULL
+);
+
+Create Role DBManagement;
+GO
+
+Grant Select, Insert, Update, Delete, Execute
+To DBManagement;
+GO
+
+DROP LOGIN DBManager;
+GO
+
+CREATE LOGIN DBManager WITH PASSWORD = 'DBManager',
+	DEFAULT_DATABASE = D3BuildMark;
+GO
+
+CREATE USER DBManager FOR LOGIN DBManager;
+GO
+
+Alter Role DBManagement Add Member DBManager;
+GO
---
+++ Documentation/Database/dbStoredProcs.sql
@@ -0,0 +1,27 @@
+Use D3BuildMark;
+GO
+
+IF OBJECT_ID('spAddUserProfile') IS NOT NULL
+DROP PROC spAddUserProfile
+GO
+
+CREATE PROC spAddUserProfile
+@UserName		varchar(40),
+@Password		varchar(40),
+@Email			varchar(40),
+@Battletag		varchar(40)
+AS 
+BEGIN TRANSACTION
+INSERT INTO UserProfile (UserName, Password, Email, Battletag) VALUES (@UserName, @Password, @Email, @Battletag)
+COMMIT
+GO
+
+IF OBJECT_ID('spReadUserProfile') IS NOT NULL
+DROP PROC spReadUserProfile
+GO
+
+CREATE PROC spReadUserProfile
+@UserName	varchar(40)
+AS 
+SELECT UserName, Password, Email, Battletag FROM UserProfile WHERE UserName = @UserName
+GO
